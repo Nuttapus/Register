9f1cd68f9508837e611d0af30b3b27b04e945497
Nuttapus <59160561@go.buu.ac.th>
2018-09-10 23:45:17 +0700

    Finish by ken or TEeriFlE

---
+++ regisbyken/ProjectOX/Register/Register/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace Register
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.boxusername = new System.Windows.Forms.TextBox();
+            this.boxpassword = new System.Windows.Forms.TextBox();
+            this.boxname = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.okbut = new System.Windows.Forms.PictureBox();
+            this.cancelbut = new System.Windows.Forms.PictureBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.okbut)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cancelbut)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(480, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(20, 20);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(454, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(20, 20);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.White;
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(129, 12);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(250, 100);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(9, 150);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            // 
+            // boxusername
+            // 
+            this.boxusername.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.boxusername.Location = new System.Drawing.Point(234, 150);
+            this.boxusername.Name = "boxusername";
+            this.boxusername.Size = new System.Drawing.Size(190, 26);
+            this.boxusername.TabIndex = 4;
+            // 
+            // boxpassword
+            // 
+            this.boxpassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.boxpassword.Location = new System.Drawing.Point(234, 201);
+            this.boxpassword.Name = "boxpassword";
+            this.boxpassword.Size = new System.Drawing.Size(190, 26);
+            this.boxpassword.TabIndex = 5;
+            // 
+            // boxname
+            // 
+            this.boxname.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.boxname.Location = new System.Drawing.Point(234, 250);
+            this.boxname.Name = "boxname";
+            this.boxname.Size = new System.Drawing.Size(190, 26);
+            this.boxname.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Cooper Black", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(115, 150);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 21);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Username:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Cooper Black", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(115, 198);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 21);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Password:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Cooper Black", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(115, 250);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 21);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Name:";
+            // 
+            // okbut
+            // 
+            this.okbut.Image = ((System.Drawing.Image)(resources.GetObject("okbut.Image")));
+            this.okbut.Location = new System.Drawing.Point(144, 301);
+            this.okbut.Name = "okbut";
+            this.okbut.Size = new System.Drawing.Size(70, 70);
+            this.okbut.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.okbut.TabIndex = 10;
+            this.okbut.TabStop = false;
+            this.okbut.Click += new System.EventHandler(this.okbut_Click);
+            // 
+            // cancelbut
+            // 
+            this.cancelbut.Image = ((System.Drawing.Image)(resources.GetObject("cancelbut.Image")));
+            this.cancelbut.Location = new System.Drawing.Point(309, 301);
+            this.cancelbut.Name = "cancelbut";
+            this.cancelbut.Size = new System.Drawing.Size(70, 70);
+            this.cancelbut.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.cancelbut.TabIndex = 11;
+            this.cancelbut.TabStop = false;
+            this.cancelbut.Click += new System.EventHandler(this.cancelbut_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(65, 161);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(10, 10);
+            this.dataGridView1.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(500, 400);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.cancelbut);
+            this.Controls.Add(this.okbut);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.boxname);
+            this.Controls.Add(this.boxpassword);
+            this.Controls.Add(this.boxusername);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Register";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.okbut)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cancelbut)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.TextBox boxusername;
+        private System.Windows.Forms.TextBox boxpassword;
+        private System.Windows.Forms.TextBox boxname;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox okbut;
+        private System.Windows.Forms.PictureBox cancelbut;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ regisbyken/ProjectOX/Register/Register/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Driver.Builders;
+using MongoDB.Driver.GridFS;
+using MongoDB.Driver.Linq;
+
+namespace Register
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Maximized;
+            this.FormBorderStyle = FormBorderStyle.None;
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void pictureBox3_SizeChanged(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Maximized;
+            this.FormBorderStyle = FormBorderStyle.None;
+        }
+
+        private void okbut_Click(object sender, EventArgs e)
+        {
+            MongoClient client = new MongoClient("mongodb://admin:a123456@ds141902.mlab.com:41902/ox");
+            MongoServer server = client.GetServer();
+            MongoDatabase database = server.GetDatabase("ox");
+            MongoCollection symbolcollection = database.GetCollection<Usermain>("User");
+            Usermain user = new Usermain();
+            BindingList<Usermain> doclist = new BindingList<Usermain>();
+            var pd = database.GetCollection<Usermain>("User");
+            var pd1 = pd.AsQueryable().Where(pd01 => pd01.Username == boxusername.Text);
+            foreach (var p in pd1)
+            {
+                doclist.Add(p);
+                Application.DoEvents();
+            }
+            dataGridView1.DataSource = doclist;
+            if (dataGridView1.Rows.Count == 0)
+            {
+                user.Username = boxusername.Text;
+                user.Password = boxpassword.Text;
+                user.User = boxname.Text;
+                /*user.picture = null;
+                user.win = "0";
+                user.draw = "0";
+                user.lose = "0";*/
+                symbolcollection.Insert(user);
+                MessageBox.Show("เพิ่มข้อมูลเรียบร้อย");
+            }
+        }
+        public class Usermain
+        {
+            public ObjectId _id { get; set; }
+            public string User
+            {
+                get; set;
+            }
+            public string Username
+            {
+                get; set;
+            }
+            public string Password
+            {
+                get; set;
+            }
+            public string picture
+            {
+                get; set;
+            }
+            public string win
+            {
+                get; set;
+            }
+            public string draw
+            {
+                get; set;
+            }
+            public string lose
+            {
+                get; set;
+            }
+        }
+
+        private void cancelbut_Click(object sender, EventArgs e)
+        {
+            boxusername.Text = string.Empty;
+            boxpassword.Text = string.Empty;
+            boxname.Text = string.Empty;
+        }
+    }
+   
+}
+
